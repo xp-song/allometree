b635dc21ec6e14d54a7fc3c8c8e3b0881a479632
xpsong <xpsong.connect@gmail.com>
2020-05-10 16:59:34 +0800

    complete basic single-species functions; add to README and website index

--- tests/testthat/test-sp_simulate.R
+++ tests/testthat/test-sp_simulate.R
@@ -1,12 +1,12 @@
-context("sp_predict")
+context("sp_simulate")
 
 
-test_that("wrong data type error", {
+test_that("Check data type", {
 
   result <- sp_modelselect(urbantrees)
 
   expect_error(
-    sp_simulate(ref_table = result$sp_models_info, models = result$sp_models, extrapolate = c(-1,0.3))
+    sp_simulate(ref_table = result$sp_models_info, models = result$sp_models, extrapolate = c(-1,0.3)) # extrapoate down to negative
     )
 
 })
